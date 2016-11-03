adee9706a5eb04b534416211a25571e1ce863ed2
noodlesen <k.lapshov@gmail.com>
2016-11-03 12:04:38 +0300

    search improve

--- mango/static/js/base.js
+++ mango/static/js/base.js
@@ -45,10 +45,46 @@ var placeSearch = new Vue({
     data: {
         needle: '',
         showingResults: false,
-        results:{}
+        results:{},
+        preSelected: false,
+        preSelectedIndex: -1
+    },
+    computed:{
+        
     },
     methods:{
+        choosePreSelected: function(){
+            if (this.preSelected){
+               //window.location = 'http://localhost:5000/place/singapore';
+
+               //$(location).attr('href', 'http://www.sitefinity.com');
+
+                //'http://localhost:5000'+this.results[this.preSelectedIndex].url;
+                //window.location.assign("http://www.mozilla.org");//'http://localhost:5000'+this.results[this.preSelectedIndex].url;
+                //console.log(this.results[this.preSelectedIndex].url);
+                //$('#search-results').trigger('click', 'a #sl'+this.preSelectedIndex);
+                $('#sl'+this.preSelectedIndex).click();
+                alert();
+                //$("#search-results").find("a:first").trigger("click");
+            }
+        },
+        elPreSelected: function(i){
+            return ((i==this.preSelectedIndex) && this.preSelected);
+        },
+        preSelect: function(i){
+            if (this.preSelected){
+                this.preSelectedIndex+=i;
+            } else if (i==1){
+                this.preSelectedIndex=0;
+            } else if (i = -1){
+                this.preSelectedIndex=this.results.length-1;
+            }
+            this.preSelected = true;
+            console.log(this.preSelectedIndex);
+        },
         checkNeedle: function(){
+            //console.log(JSON.stringify(e));
+            
             var self=this;
             if (this.needle.length>=2){
                 getResults('/json/place-search','json',{needle: this.needle}, function(res){
@@ -64,6 +100,7 @@ var placeSearch = new Vue({
             } else {
                 self.showingResults = false;
             }
+
         }
     }
 });
